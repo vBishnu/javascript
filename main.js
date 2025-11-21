313c163998582e7c62aa0a6ab9d1179dbfd73ee0
vBishnu <bishnu.projectile@gmail.com>
2025-11-21 11:19:25 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// console.log("helloWorld")
+// console.log("byeWorld")
+
+
+var bag = "books"
+var box = "cloths"
+var nepal = "mtEverest"
+var world = "continents"
+
+console.log(bag)
+console.log(box)
+console.log(nepal)
+console.log(world)
