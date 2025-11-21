cfe05aad9304f90518649880415d3a7609d74cf3
vBishnu <bishnu.projectile@gmail.com>
2025-11-21 17:53:06 +0545

    Variable, Aaaya and Object

--- main.js
+++ main.js
@@ -2,12 +2,50 @@
 // console.log("byeWorld")
 
 
-var bag = "books"
-var box = "cloths"
-var nepal = "mtEverest"
-var world = "continents"
+// VAR method
 
-console.log(bag)
-console.log(box)
-console.log(nepal)
-console.log(world)
+// var name = "Luffy"
+// var age = "19"
+// var location = "elbaf"
+// var objective = "Future_Pirate_King"
+
+// console.log(name)
+// console.log(age)
+// console.log(location)
+// console.log(objective)
+
+
+
+// ARRAY method
+// let names = ['Luffy', 19, 'Kenma', 'Future_Pirate_King']
+
+// add value to the beggining
+// names.unshift('Shanks')
+
+// // remove the first value
+// names.shift() 
+
+// // add value to the end
+// names.push('Zorro')
+
+// // remove the last value
+// names.pop()
+
+// 
+// console.log(names)
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+
+// OBJECT method
+const title1 = {
+    name : "Luffy",
+    age : 19,
+    location : "elbaf",
+    objective : "Future_Pirate_King"
+}
+
+
+console.log(title1.name, title1.age, title1.location, title1.objective)
