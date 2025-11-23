e109c358cebedbf1a3f26b3a4b97703679b71736
vBishnu <bishnu.projectile@gmail.com>
2025-11-23 13:35:24 +0545

    functions

--- Day4.js
+++ Day4.js
@@ -1,1 +1,83 @@
 // Loops
+
+// for (var i = 0; i<25; i=i+2){
+//     console.log(i,'Success')
+// }
+
+
+// var i = 1
+
+// while(i <= 15){
+//     console.log(i, 'True'); 
+//     i++
+// }
+
+
+// var person = {
+//     name: "Bishnu",
+//     address: "salakpur",
+//     age: 28
+// }
+
+
+let i = 0;
+
+do{
+    console.log(i, "congratulation")
+    i++
+   
+}
+while(i<10)
+
+
+// console.log(person.name)
+// console.log(person.address)
+// console.log(person.age)
+
+
+// var response = {
+//     file: [
+//         {
+//             _id: 1234,
+//             user: "one",
+//             name: "Bishnu",
+
+//         }
+//     ]
+// }
+
+// console.log(response.file[0].user)
+
+
+// var response2 = {
+//     status: "online",
+//     message: "hello",
+//     file: [
+//         {
+//             _id1: 1234,
+//             user1: "one",
+//             name1: "Bishnu"
+
+//         },
+//         {
+//             _id2: 2345,
+//             user2: "two",
+//             name2: "Budhahtoki"
+
+//         }
+//     ]
+// }
+
+
+// console.log(response2.file[1].name2)
+
+
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// for( var i = cars; i<cars.length; i++){
+//     console.log(i)
+// }
+
+
+// console.log(i)
---
+++ Day5.js
@@ -0,0 +1,80 @@
+// //Functions
+
+// console.log(1 + 1)
+
+// //Regular Function
+// function firstName(){
+//     console.log("Hello World")
+// }
+// firstName()
+
+
+
+// function multiply(a, b){
+//     console.log(a*b)
+// }
+// multiply(6, 5)
+
+
+//Named function
+
+// const lastName = function(){
+//     console.log("Budhahtoki")
+// }
+
+// lastName()
+
+
+// //IFEE
+// (function hello(){
+//     console.log("Hi!")
+// })()
+
+// function fullName(name="Your name here"){
+//     console.log("Hello" + name)
+// }
+
+// fullName("Bishnu") 
+
+// function fullName(name="Your name here"){
+//     console.log(`Hello ${name}`)
+// }
+
+// fullName("Bishnu") 
+
+
+// const data2=(age=0)=>{
+//     console.log(age + " years old")
+// }
+// data2()
+
+
+
+
+// let schools = [1,2,3,4,5,6,7,8,9,10]
+
+// schools.forEach((school)=>{
+//     console.log(school)
+// })
+
+
+let names =["Bishnu", "Sita", "Krishna", "Parbati"]
+
+
+
+let alias = names.forEach((name)=>{
+   alias.push(name +" Budhahtoki")
+})
+
+console.log(alias)
+
+
+// let names =["Bishnu", "Sita", "Krishna", "Parbati"]
+
+// let alias = names.map((name)=>{
+//     return name + " Budhahtoki"
+// })
+
+// console.log(alias)
+
+
